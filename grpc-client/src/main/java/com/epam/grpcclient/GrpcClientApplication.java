47e0a27d05acbe0ca39202e727f241c3f797fa95
DuongVu98 <duongvutung1998@gmail.com>
2021-06-27 17:25:01 +0700

    add golang server

--- grpc-client/src/main/java/com/epam/grpcclient/GrpcClientApplication.java
+++ grpc-client/src/main/java/com/epam/grpcclient/GrpcClientApplication.java
@@ -14,20 +14,48 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 public class GrpcClientApplication {
 
-    @GrpcClient("")
-    private EventProducerGrpc.EventProducerBlockingStub producerBlockingStub;
+    @GrpcClient("local-grpc-server")
+    private EventProducerGrpc.EventProducerBlockingStub localServer;
+
+    @GrpcClient("cloud-grpc-server")
+    private EventProducerGrpc.EventProducerBlockingStub cloudClient;
+
+    @GrpcClient("golang-grpc-server")
+    private EventProducerGrpc.EventProducerBlockingStub golangClient;
 
     public static void main(String[] args) {
         SpringApplication.run(GrpcClientApplication.class, args);
     }
 
-    @GetMapping
+    @GetMapping(value = "/local")
     public String sendUpdateRequest() {
         var command = ExternalService.UpdateCommand.newBuilder()
            .setUserId("user-1")
            .setInfo("user info")
            .build();
-        var response = producerBlockingStub.sendCommand(command);
+        var response = localServer.sendCommand(command);
+
+        return response.getMessage();
+    }
+
+    @GetMapping(value = "/cloud")
+    public String sendToCloudServer(){
+        var command = ExternalService.UpdateCommand.newBuilder()
+           .setUserId("user-1")
+           .setInfo("user info")
+           .build();
+        var response = cloudClient.sendCommand(command);
+
+        return response.getMessage();
+    }
+
+    @GetMapping(value = "/go")
+    public String sendToGolangServer(){
+        var command = ExternalService.UpdateCommand.newBuilder()
+           .setUserId("user-1")
+           .setInfo("user info")
+           .build();
+        var response = golangClient.sendCommand(command);
 
         return response.getMessage();
     }
